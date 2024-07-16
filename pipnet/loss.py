68cef0dea7fabc12eb2729e4e24a56230edc9c8b
Adam Wrobel <adamwrobel@Adams-MBP.lan>
2024-07-16 15:02:26 +0200

    epoch idx reset fix

--- pipnet/loss.py
+++ pipnet/loss.py
@@ -282,7 +282,6 @@ class WeightedSumLoss(torch.nn.Module):
         return self.summed_val / self.num_steps
 
     def reset(self):
-        self.curr_epoch = 0
         self.summed_val = 0.0
         self.num_steps = 0
 
